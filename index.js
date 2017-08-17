5fd3438c16fb455a27c690012789c0cc71ebbcae
Guillaume Wuip <guillaume@wuips.com>
2017-08-17 23:13:44 +0200

    Get list of favorite artists

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 
+'use strict';
+
 import fetch from 'isomorphic-fetch';
 import queryString from 'query-string';
-import Rx from 'rxjs';
+import { Observable } from 'rxjs';
 
 const API_KEY = '5a95a0ce3ef544f937afd572ae8ed901';
 const API_ROOT = 'http://ws.audioscrobbler.com/2.0/';
+
+const USER_NAME = 'guillaumewuip';
+
+// API
 
 const getFavoritesSongsPage = (userId, page = 1, limit = 100) => {
   const query = queryString.stringify({
@@ -13,30 +19,93 @@ const getFavoritesSongsPage = (userId, page = 1, limit = 100) => {
     api_key: API_KEY,
     format:  'json',
     limit,
-    page
+    page,
   });
 
-  return Rx.Observable.create((observer) => {
+  return Observable.fromPromise(
     fetch(`${API_ROOT}?${query}`)
       .then((response) => {
-        console.log('ok');
         if (response.status >= 400) {
           throw new Error("Bad response from server");
         }
         return response.json();
       })
-      .then((result) => observer.next(result));
+      .then((body) => {
+        if (body.error) {
+          throw new Error(body.message);
+        }
+        return body;
+      })
+  );
+};
+
+const getArtistInfo = (name, mbid, username = undefined, lang = 'en') => {
+  const query = queryString.stringify({
+    method:  'artist.getInfo',
+    name,
+    mbid,
+    lang,
+    username,
+    api_key: API_KEY,
+    format:  'json',
   });
+
+  return Observable.fromPromise(
+    fetch(`${API_ROOT}?${query}`)
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error('Bad response from server');
+        }
+        return response.json();
+      })
+      .then((body) => {
+        if (body.error) {
+          throw new Error(body.message);
+        }
+        return body;
+      })
+  );
 };
 
-const getFavoritesSongs = (userId) => {
-  return getFavoritesSongsPage(userId)
+// Logic
+
+const getFavoritesSongs = (userId, page = 1) => (
+  getFavoritesSongsPage(userId, page)
     .flatMap((response) => {
-      const result = Rx.Observable.fromObject(response);
-      console.log(result);
-      return 'toto';
-    });
-};
+      const
+        tracks      = response.lovedtracks.track,
+        currentPage = response.lovedtracks['@attr'].page,
+        totalPage = response.lovedtracks['@attr'].totalPages;
 
-getFavoritesSongs('guillaumewuip');
+      const result$ = Observable.from(tracks);
+
+      return result$.concat(
+        currentPage >= totalPage
+          ? Observable.empty()
+          : getFavoritesSongs(userId, page + 1)
+      );
+    })
+);
+
+const getArtistFromSong = (song) => song.artist;
+
+getFavoritesSongs(USER_NAME)
+  .map(getArtistFromSong)
+  .filter((artist) => artist.url)
+  .distinct((artist) => artist.url)
+  .flatMap((artist) => (
+    getArtistInfo(
+      artist.name,
+      artist.mbid,
+      USER_NAME
+    )
+    .catch(() => Observable.empty())
+  ))
+  .subscribe(
+    (desc) => {
+      console.log(desc);
+    },
+    (err) => console.error(err),
+    () => {}
+  );
 
