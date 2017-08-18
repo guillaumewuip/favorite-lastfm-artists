8c8d7e4a73b338cf4895fe34fabbe389bf4fc65b
Guillaume Wuip <guillaume@wuips.com>
2017-08-18 09:16:33 +0200

    Fix webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -4,26 +4,43 @@ var webpack = require('webpack');
 module.exports = {
   entry: './src/app.jsx',
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'app.bundle.js',
+    path:       path.resolve(__dirname, 'build'),
+    publicPath: 'build',
+    filename:   'app.bundle.js',
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.jsx?$/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['es2015'],
-        },
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                'es2017',
+                'react'
+              ],
+            },
+        }
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
       }
     ],
   },
-  stats: {
-    colors: true
-  },
   devtool: 'source-map',
   devServer: {
-    compress: true,
-    port: 9000,
+    compress:         true,
+    port:             9000,
+    contentBase:      path.join(__dirname),
+    compress:         true,
+    watchContentBase: true,
   },
 };
