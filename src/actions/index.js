ae8116f452770ec3fcc4b9b8c8bbc32a081978eb
Guillaume Wuip <guillaume@wuips.com>
2017-08-20 00:19:30 +0200

    Request info for each artist

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,3 +4,6 @@ export const loadFavoriteTracks = () => ({ type: LOAD_FAVORITE_TRACKS });
 
 export const ADD_NEW_TRACKS = 'ADD_NEW_TRACKS';
 export const addNewTracks = (tracks) => ({ type: ADD_NEW_TRACKS, tracks });
+
+export const ADD_NEW_ARTIST_INFO = 'ADD_NEW_ARTIST_INFO';
+export const addNewArtistInfo = (info) => ({ type: ADD_NEW_ARTIST_INFO, info });
--- src/epics/api.js
+++ src/epics/api.js
@@ -4,37 +4,63 @@ import { Observable } from 'rxjs/Observable';
 
 import {
   LOAD_FAVORITE_TRACKS,
-  addNewTracks
+  addNewTracks,
+  addNewArtistInfo
 } from '../actions';
 
-export const getFavoritesTracks = (client) => (userId, page = 1) => (
-  client.getFavoritesTracks(userId, page)
-    .flatMap((response) => {
-      const
-        tracks      = response.lovedtracks.track,
-        currentPage = parseInt(response.lovedtracks['@attr'].page, 10),
-        totalPage   = parseInt(response.lovedtracks['@attr'].totalPages, 10);
+export const getFavoritesTracks = (client) => (userId, page = 1) => client
+  .getFavoritesTracks(userId, page)
+  .mergeMap((response) => {
+    const
+      tracks      = response.lovedtracks.track,
+      currentPage = parseInt(response.lovedtracks['@attr'].page, 10),
+      totalPage   = parseInt(response.lovedtracks['@attr'].totalPages, 10);
 
-      const result$ = Observable.from([tracks]);
+    const result$ = Observable.from([tracks]);
 
-      return result$.concat(
-        currentPage >= totalPage
-          ? Observable.empty()
-          : getFavoritesTracks(client)(userId, page + 1)
-      );
-    })
-);
+    return result$.concat(
+      currentPage >= totalPage
+        ? Observable.empty()
+        : getFavoritesTracks(client)(userId, page + 1)
+    );
+  });
+
+export const getArtistInfo = (client) => (name, mbid, userId = '') => client
+  .getArtistInfo(
+    name,
+    mbid,
+    userId
+  );
 
 export const LOAD_FAVORITE_TRACKS_SUCCESS = 'LOAD_FAVORITE_TRACKS_SUCCESS';
 export const loadFavoriteTracksOnInitEpics = (action$, store, { lastFM }) =>
   action$
     .ofType(LOAD_FAVORITE_TRACKS)
-    .flatMap(() => getFavoritesTracks(lastFM)('guillaumewuip')
+    .mergeMap(() => getFavoritesTracks(lastFM)('guillaumewuip') // TODO
       .filter((result) => !!result)
       .map(addNewTracks)
       .concat(Observable.of({ type: LOAD_FAVORITE_TRACKS_SUCCESS }))
     );
 
+export const loadFavoriteArtistsInfo = (action$, store, { lastFM }) =>
+  action$
+    .ofType(LOAD_FAVORITE_TRACKS_SUCCESS)
+    .map(() => store.getState())
+    .mergeMap((state) => Observable
+      .from(state.get('artists').toArray())
+      .mergeMap((artist) => getArtistInfo(lastFM)(
+        artist.get('name'),
+        artist.get('mbid'),
+        'guillaumewuip' // TODO
+      )
+        // don't break stream if unknown artist
+        .catch(() => Observable.empty())
+      )
+    )
+    .do((info) => console.log('info', info))
+    .map(addNewArtistInfo);
+
 export default combineEpics(
-  loadFavoriteTracksOnInitEpics
+  loadFavoriteTracksOnInitEpics,
+  loadFavoriteArtistsInfo
 );
